9ae948b95dd2bd9dfab0f96ee3a0b2d5cb2790dd
mkuchin <mkuchin@gmail.com>
2016-04-13 22:01:30 +1000

    upgrade to grails 2.5.4

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,8 +30,8 @@ environments {
   }
   production {
     dataSource {
-      dbCreate = "update"
-      url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+      dbCreate = "create-drop" //todo: update
+      url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;" //DB_CLOSE_ON_EXIT=FALSE"
       properties {
         // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
         jmxEnabled = true
