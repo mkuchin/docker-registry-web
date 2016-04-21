932fe709c4668cb9cf3932e5f7eb2bcc31cb80e5
mkuchin <mkuchin@gmail.com>
2016-04-21 14:19:23 +1000

    disable yaml factory

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,12 @@
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
-import org.springframework.core.io.PathResource
-
 // Place your Spring DSL code here
 beans = {
+/*
   yamlConfig(YamlPropertiesFactoryBean) {
     setProperty('resources', new PathResource('grails-app/conf/config.yml'))
   }
+
+  propertyConfig(PropertySourcesPlaceholderConfigurer) {
+    properties = ref('yamlConfig')
+  }
+  */
 }
