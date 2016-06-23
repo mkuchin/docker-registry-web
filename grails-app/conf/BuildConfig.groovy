912a779346ce03f7591778a75c3d83bf0540c8c7
mkuchin <mkuchin@gmail.com>
2016-06-23 19:04:36 +1000

    exclude grails docs to prevent two versions of bouncycastle

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,6 +27,7 @@ grails.project.dependency.resolution = {
   inherits("global") {
     // specify dependency exclusions here; for example, uncomment this to disable ehcache:
     // excludes 'ehcache'
+    excludes "grails-docs"
   }
   log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
   checksums true // Whether to verify checksums on resolve
