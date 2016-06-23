02961008a617f005a6a673be4b4705a4e86c1cf6
mkuchin <mkuchin@gmail.com>
2016-06-23 19:03:49 +1000

    PrivateKeyInfo key type support

--- src/groovy/docker/registry/web/PemUtils.groovy
+++ src/groovy/docker/registry/web/PemUtils.groovy
@@ -1,6 +1,13 @@
 package docker.registry.web
 
 import org.apache.commons.codec.binary.Base32
+import org.bouncycastle.asn1.DERNull
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
+import org.bouncycastle.asn1.pkcs.RSAPrivateKey
+import org.bouncycastle.asn1.pkcs.RSAPublicKey
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
 import org.bouncycastle.openssl.PEMKeyPair
 import org.bouncycastle.openssl.PEMParser
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
@@ -8,13 +15,8 @@ import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
 import java.security.KeyPair
 import java.security.MessageDigest
 import java.security.PublicKey
-import java.security.interfaces.RSAPrivateKey
 
 class PemUtils {
-  public static RSAPrivateKey generatePrivateKeyFromPEM(File keyFile) {
-    def kp = getKeyPair(keyFile)
-    return (RSAPrivateKey) kp.private
-  }
 
   public static getKeyDigest(PublicKey publicKey) {
     def digest = MessageDigest.getInstance("SHA-256").digest(publicKey.encoded)
@@ -27,9 +29,24 @@ class PemUtils {
   public static KeyPair getKeyPair(File file) {
     Object object = getPemObject(file)
     JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC")
-    return converter.getKeyPair((PEMKeyPair) object)
+    PEMKeyPair pemKeyPair
+    if (object instanceof PrivateKeyInfo) {
+      pemKeyPair = getPemKeyPair(object)
+    } else if (object instanceof PEMKeyPair) {
+      pemKeyPair = object
+    } else {
+      throw new RuntimeException("Key file type not supported: ${object.class}")
+    }
+    return converter.getKeyPair(pemKeyPair)
   }
 
+  private static PEMKeyPair getPemKeyPair(PrivateKeyInfo pk) {
+    def rsaPk = RSAPrivateKey.getInstance(pk.parsePrivateKey())
+    def pubSpec = new RSAPublicKey(rsaPk.getModulus(), rsaPk.getPublicExponent())
+    AlgorithmIdentifier algId = new AlgorithmIdentifier(PKCSObjectIdentifiers.rsaEncryption, DERNull.INSTANCE)
+    def pemKeyPair = new PEMKeyPair(new SubjectPublicKeyInfo(algId, pubSpec), new PrivateKeyInfo(algId, rsaPk))
+    pemKeyPair
+  }
 
   private static Object getPemObject(File file) {
     PEMParser pemParser = new PEMParser(new FileReader(file));
